4a61ea6fbee47a922d82dce71342666faaebfeaf
jainenb <jainenbanqueroalbornoz@gmail.com>
2024-12-17 18:30:33 -0300

    cambios en estructura html y hojas de estilo index y style, agregue archivos json y ejs

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,9 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+ 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const adoptionRouter = require('./routes/adoption');
+
+app.use('/adoption', adoptionRouter);
+
 
 var app = express();
 
@@ -21,6 +25,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', adoptionRouter);
+
+// Nueva ruta para manejar los detalles de los animales
+app.get('/adoptDetail', (req, res) => {
+  const animalId = req.query.id; // Captura el parámetro id de la URL
+  if (animalId) {
+    res.render('adoptDetail', { id: animalId }); // Renderiza la vista adoptDetail.ejs y pasa el id
+  } else {
+    res.status(400).send('ID no proporcionado');
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
